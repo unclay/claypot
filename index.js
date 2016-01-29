be051f211c37d6a9fe5daf3b4e84bdf7e36d5526
vagrant <vagrant@precise64.(none)>
2016-01-29 16:16:52 +0800

    test pgsql

--- index.js
+++ index.js
@@ -18,11 +18,30 @@ app.get('/version', function(req, res){
 				type:              os.type(),
 				cpus:              os.cpus()
 			},
+			env: {
+				PORT:         process.env.PORT || 0,
+				DATABASE_URL: process.env.DATABASE_URL || ''
+			},
 			node: {
 				version: process.version
 			}
 		}
 	});
+});
+
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err){
+  	return console.log( err );
+  } 
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(43, JSON.stringify(row));
+    });
 });
 
 app.use('/', express.static(__dirname + '/views'));
