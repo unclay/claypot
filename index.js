22b963be7539fcc4289143d7d048bbea98ac8742
wangchenglin <wcl58981298@gmail.com>
2016-02-18 23:41:56 +0800

    新增手机厂商统计

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
-const koa          = require('koa');
-const fs           = require('co-fs');
-const router       = require('koa-router')();
-const koaPg        = require('koa-pg');
-const staticCache  = require('koa-static-cache');
-const path         = require('path');
-const os           = require('os');
-const moment       = require('moment');
+let koa          = require('koa');
+let fs           = require('co-fs');
+let router       = require('koa-router')();
+let koaPg        = require('koa-pg');
+let staticCache  = require('koa-static-cache');
+let path         = require('path');
+let os           = require('os');
+let moment       = require('moment');
 
-const app = koa();
-
-
+let app = koa();
 
 const port = process.env.PORT || 1122;
+
+let   cache = {};
 
 // logger
 app.use(function *(next){
@@ -77,20 +77,103 @@ router.get('/c.gif', function *(next){
 });
 
 router.get('/analysis', function *(next){
-	console.log( this.query.start, this.query.end );
-	let start   = this.query.start;
-	let end     = this.query.end;
-	let result  = yield this.pg.db.client.query_(`SELECT * FROM analysis WHERE date BETWEEN ${this.query.start} and ${this.query.end}`);
-	let dataObj = {};
-	for(let item of result.rows){
-		let dateYMD = moment.unix(item.date).format('YYYY-MM-DD');
-		dataObj[ dateYMD ] = dataObj[ dateYMD ] || 0;
-		dataObj[ dateYMD ]++;
+	if( !!cache.analysis && cache.analysis.expire > moment().format('X') ){
+		this.body = {
+			error_code: 0,
+			data: cache.analysis.content
+		};
+	} else {
+		let start   = this.query.start;
+		let end     = this.query.end;
+		let result  = yield this.pg.db.client.query_(`SELECT * FROM analysis WHERE date BETWEEN ${this.query.start} and ${this.query.end}`);
+		let dataObj = {
+			pv: {},
+			os: {},
+			manufacturers: {},
+			viewport: {}
+		};
+		const os = {
+			android: function(u){ return u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; },
+			ios:     function(u){ return !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); },
+			other:   function(u){ return true; }
+		}
+		const manufacturers = {
+			//pc:      /windows|Macintosh/ig, // pc
+			iPhone:  /iphone/ig,
+			iPod:    /ipod/ig,
+			iPad:    /ipad/ig,
+			SAMSUNG: /GT-|SM-|SCH-/ig,
+			MI:      /HM|RedMi|Mi/ig, // 小米
+			Huawei:  /huawei|honor/ig,
+			vivo:    /vivo/ig, 
+			Google:  /nexus/ig, 
+			Nokia:   /Nokia/ig,
+			oppo:    /oppo/ig, // 完全没有正则可以匹配到，超级不完整的数据
+			one:     /one/ig,  // 一加
+			asus:    /asus/ig, // 华硕
+			htc:     /htc/ig,  // 
+			lenovo:  /lenovo/ig,
+			meitu:   /meitu/ig, // 美图
+			zte:     /zte/ig,   // 中兴
+			//other:   /.*/ig
+		}
+		for(let item of result.rows){
+			// pv
+			let dateYMD = moment.unix(item.date).format('YYYY-MM-DD');
+			dataObj.pv[ dateYMD ] = dataObj.pv[ dateYMD ] || 0;
+			dataObj.pv[ dateYMD ]++;
+			// manufacturers
+			for(let mf in manufacturers){
+				if( new RegExp(manufacturers[mf]).test(item.ua) ){
+					dataObj.manufacturers[mf] = dataObj.manufacturers[mf] || 0;
+					dataObj.manufacturers[mf]++;
+					break;
+				}
+			}
+			// os
+			for(let i in os){
+				dataObj.os[i] = dataObj.os[i] || 0;
+				if( os[i](item.ua) ){
+					dataObj.os[i]++;
+					break;
+				}
+			}
+			// viewport
+			let vp = item.ua.match(/\([^\)]*\)/i);
+			if( !!vp ){
+				vp = vp[0].replace(/[\(\)]/g, '');
+				dataObj.viewport[vp] = dataObj.viewport[vp] || 0;
+				dataObj.viewport[vp]++;
+			}
+			
+			// .match(/\([^\)]*\)/i)
+
+		}
+
+		let body = {}
+		// 数据重构成数组
+		for(let i in dataObj){
+			for(let j in dataObj[i]){
+				body[i] = body[i] || [];
+				body[i].push({
+					name:  j,
+					value: dataObj[i][j]
+				});
+			}
+		}
+
+		// 缓存数据，数据库连接慢（暂定一个小时）
+		cache.analysis = {
+			expire: parseInt(moment().format('X'), 10) + 60*60,
+			content: body
+		}
+
+		this.body = {
+			error_code: 0,
+			data: body
+		};
 	}
-	this.body = {
-		error_code: 0,
-		data: dataObj
-	};
+	
 });
 
 router.get('/sync', function *(next){
@@ -127,9 +210,9 @@ router.get('/sync', function *(next){
 
 app.use(router.routes());
 
-app.use(staticCache( __dirname + '/views', {
+app.use(staticCache(path.join(__dirname, 'views'), {
 	maxAge: 365 * 24 * 60 * 60
-}));
+}))
 
 app.listen(port, function(){
 	console.log(`listen to ${port}`);
